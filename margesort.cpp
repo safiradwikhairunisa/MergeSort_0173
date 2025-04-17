f40f8f3e27e4babe38005904bf9c02d8e6109f28
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-04-17 11:11:48 +0700

    step 5

--- margesort.cpp
+++ margesort.cpp
@@ -82,4 +82,20 @@ void mergeSort(int low, int high)
         i++;  //step 4f.ii
         k++;  //step 4f.iii
     }
+    
+    //step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
+
+void output()
+{
+    cout << "\nData setelah diurutkan (mergeSort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
